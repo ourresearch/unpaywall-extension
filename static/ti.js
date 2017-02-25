a7d724ee7ff8169cb3d0c4014221606b300790f0
Jason Priem <jp@jasonpriem.org>
2017-02-24 19:51:49 -0800

    more faqs.

--- static/ti.js
+++ static/ti.js
@@ -336,41 +336,109 @@ angular.module("landing.tpl.html", []).run(["$templateCache", function($template
     "        <div class=\"content\">\n" +
     "            <h2>About</h2>\n" +
     "            <p>\n" +
-    "                Ever get stopped by a paywall from reading a scholarly article?\n" +
-    "                UnPaywall is here to help.\n" +
+    "                Ever find that perfect scholarly article, only to get stopped by a paywall?\n" +
+    "                We can help.\n" +
+    "            </p>\n" +
+    "            <p>\n" +
     "                <!--\n" +
     "                It’s easy to use: just install the browser extension\n" +
     "                from the Chrome App store, and keep browsing like you normally do.\n" +
     "                -->\n" +
     "\n" +
-    "                Every time you view a paywalled article, we’ll automatically\n" +
-    "                look for a copy in our index\n" +
+    "                Every time you hit a paywalled article, Unpaywall automatically\n" +
+    "                looks for a copy in our index\n" +
     "                of over 20 million free, legal fulltext PDFs.\n" +
-    "                If we find one, we give you a link and you read. Open access has never been easier.\n" +
+    "                If we find one, we give you a link to read. Open access has never been easier.\n" +
     "            </p>\n" +
     "        </div>\n" +
     "        <div class=\"content\">\n" +
     "            <h2>FAQ</h2>\n" +
     "            <dl>\n" +
+    "                <dt>Who is behind Unpaywall?</dt>\n" +
+    "                <dd>\n" +
+    "                    Unpaywall is from <a href=\"http://impactstory.org\">Impactstory,</a>\n" +
+    "                    a nonprofit working to supercharge science by making it\n" +
+    "                    more open, reusable, and web-native.\n" +
+    "                </dd>\n" +
+    "\n" +
+    "                <dt>Why did you make it?</dt>\n" +
+    "                <dd>\n" +
+    "                    We got tired of running into paywalled articles! The worst part was, we knew\n" +
+    "                    that millions of researchers are currently uploading their own fulltext PDFs\n" +
+    "                    to <a href=\"https://en.wikipedia.org/wiki/Preprint\">preprint servers</a>\n" +
+    "                    and <a href=\"https://en.wikipedia.org/wiki/Institutional_repository\">institutional repositories,</a>\n" +
+    "                    worldwide, making them free for anyone to read. There was just no easy \n" +
+    "                    way to find them, especially not one that was integrated into our \n" +
+    "                    reading workflows. So we made one! In the long term, we think tools like\n" +
+    "                    Unpaywall will nurture the transition to a fully\n" +
+    "                    <a href=\"https://en.wikipedia.org/wiki/Open_access\">open access</a>\n" +
+    "                    scholarly publishing model, by closing the gap between readers and\n" +
+    "                    freely-available fulltext.\n" +
+    "\n" +
+    "                </dd>\n" +
+    "\n" +
+    "                <dt>How do I use this?</dt>\n" +
+    "                <dd>\n" +
+    "                    There's nothing to it. After you\n" +
+    "                    <a href=\"https://chrome.google.com/webstore/detail/unpaywall/iplffkdpngmdjhlpjmppncnlhomiipha\">install the extension</a>\n" +
+    "\n" +
+    "                    we'll do the work in the background. When you view a paywalled article,\n" +
+    "                    we'll search for a free legal copy. If we find one, you'll see a bright green\n" +
+    "                    \"free\" tab on the right side of your browser window. Click on the tab, read the\n" +
+    "                    article. If you want to see an example of Unpaywall working,\n" +
+    "                    visit <a href=\"http://www.sciencedirect.com/science/article/pii/S0140673611617522\">this article</a>\n" +
+    "                    after you've installed the extension.\n" +
+    "                </dd>\n" +
+    "\n" +
+    "                <dt>How often does Unpaywall actually find a free PDF for a given article?</dt>\n" +
+    "                <dd>\n" +
+    "                    Unfortunately not all authors upload their fulltext PDFs online&mdash;although\n" +
+    "                    the number is growing every day. So sometimes there's no fulltext available online,\n" +
+    "                    and Unpaywall comes up empty. The exact percentage depends on the age\n" +
+    "                    and field of the article, but we generally find around 40% success.\n" +
+    "                    There's actually been a bunch of research on this recently, and we're planning a blog\n" +
+    "                    post breaking it down&mdash;stay tuned.\n" +
+    "\n" +
+    "                </dd>\n" +
+    "\n" +
     "                <dt>Where does your data come from?</dt>\n" +
-    "                <dt>\n" +
+    "                <dd>\n" +
+    "                    We gather information from thousands of different open-access repositories, with the\n" +
+    "                    help of a number of partners. In particular, we depend on data from\n" +
+    "                    <a href=\"https://doaj.org/\">the DOAJ,</a>\n" +
+    "                    <a href=\"https://www.crossref.org/\">Crossref,</a>\n" +
+    "                    <a href=\"https://www.datacite.org/\">DataCite,</a>\n" +
+    "                    and (especially) <a href=\"https://www.base-search.net/\">BASE.</a>\n" +
+    "                    All this data is available for free via the API of another Impactstory project,\n" +
+    "                    <a href=\"http://oadoi.org\">oaDOI.</a>\n" +
     "\n" +
-    "                </dt>\n" +
-    "                <dt>Who are you, and why are you doing this?</dt>\n" +
-    "                <dt>\n" +
+    "                    The oaDOI API is fully free,\n" +
+    "                    open, and scalable and lots of folks are building cool stuff on it already...check\n" +
+    "                    it out if you'd like to build something too!\n" +
+    "                </dd>\n" +
     "\n" +
-    "                </dt>\n" +
-    "                <dt>How often does UnPaywall actually find a free PDF for a given article?</dt>\n" +
-    "                <dt>\n" +
-    "\n" +
-    "                </dt>\n" +
     "                <dt>How is this different from Sci-Hub?</dt>\n" +
-    "                <dt>\n" +
+    "                <dd>\n" +
+    "                    Like Unpaywall, Sci-Hub finds fulltext PDFs for paywalled articles. The main difference\n" +
+    "                    is where those PDFs come from: Unpaywall finds PDFs legally uploaded\n" +
+    "                    by the authors themselves, while\n" +
+    "                    Sci-Hub uses PDFs that are obtained by other means including automated web scraping\n" +
+    "                    of publisher sites. Sci-Hub's method delivers more comprehensive results,\n" +
+    "                    but is probably not super legal.\n" +
+    "                    <a href=\"http://blog.impactstory.org/comparing-sci-hub-oadoi/\">We're not against Sci-Hub.</a>\n" +
+    "                    But we  think Unpaywall offers a more sustainable approach, by working within copyright law\n" +
+    "                    and supporting the growing open access movement.\n" +
+    "                </dd>\n" +
     "\n" +
-    "                </dt>\n" +
+    "\n" +
     "                <dt>I found a bug</dt>\n" +
     "                <dt>\n" +
-    "\n" +
+    "                    Sorry about that! Unfortunately this is not uncommon, mostly because\n" +
+    "                    scholarly publishing is still sort of a wild west of missing and ignored\n" +
+    "                    information standards, and this creates a number of problems for automatic systems like\n" +
+    "                    Unpaywall. The good news is that it's still early days for this project and\n" +
+    "                    it's under very active development. When you find an error,\n" +
+    "                    <a href=\"mailto:team@impactstory.org\">drop us an email</a> and we'll get it fixed for you.\n" +
     "                </dt>\n" +
     "\n" +
     "            </dl>\n" +

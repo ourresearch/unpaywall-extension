18bc72cc9cab98a33614875d6aece9027269df2f
Jason Priem <jp@jasonpriem.org>
2017-02-28 19:04:42 -0800

    bugfix devlog call in iframe.

--- extension/inside-frame.js
+++ extension/inside-frame.js
@@ -1,7 +1,6 @@
 $(".button.success")
     .fadeIn()
     .click(function(){
-        devLog("click on the iframe button!")
         parent.postMessage({
             unpaywall: "go-to-pdf"
         }, "*")

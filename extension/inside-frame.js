1e9fe31d1720328992c08ad529814fab24bc495f
Jason Priem <jp@jasonpriem.org>
2017-02-28 16:26:00 -0800

    integrates PDF scrape. largely working but not yet.

--- extension/inside-frame.js
+++ extension/inside-frame.js
@@ -5,19 +5,33 @@ var devLog = function(str, obj){
     }
 }
 
-window.addEventListener("message", receiveMessage, false);
-function receiveMessage(msg){
-    if  (msg.data.unpaywall == "success"){
-        devLog("success")
-        $(".button.success")
-            .fadeIn()
-            .click(function(){
-                devLog("click!", msg.data.unpaywall.url)
-                parent.postMessage({
-                    unpaywall: "go-to-pdf"
-                }, "*")
-            })
-    }
+devLog("from the iframe: name=", window.name)
+$(".button.success")
+    .fadeIn()
+    .click(function(){
+        devLog("click on the iframe button!")
+        parent.postMessage({
+            unpaywall: "go-to-pdf"
+        }, "*")
+    })
+
+$(".button").addClass(window.name)
+
+
+
+//window.addEventListener("message", receiveMessage, false);
+//function receiveMessage(msg){
+//    if  (msg.data.unpaywall == "success"){
+//        devLog("success")
+//        $(".button.success")
+//            .fadeIn()
+//            .click(function(){
+//                devLog("click!", msg.data.unpaywall.url)
+//                parent.postMessage({
+//                    unpaywall: "go-to-pdf"
+//                }, "*")
+//            })
+//    }
 
     //if (msg.data.unpaywall == "failure") {
     //    $(".button")
@@ -31,4 +45,4 @@ function receiveMessage(msg){
 
 
 
-}
+//}
--- extension/unpaywall.js
+++ extension/unpaywall.js
@@ -2,6 +2,25 @@ console.log("i am the unpaywall main extension.")
 
 var devMode = true;
 
+
+// global variables:
+var iframe = document.createElement('iframe');
+var results = {
+    pdfScrape: {
+        url: undefined,
+        isComplete: false,
+        color: undefined
+    },
+    oadoi: {
+        url: undefined,
+        isComplete: false,
+        color: undefined
+    }
+}
+var iframeIsInserted = false
+
+
+
 var doiMetaNames = [
     "citation_doi",
     "doi",
@@ -18,29 +37,6 @@ var devLog = function(str, obj){
     }
 }
 
-
-function createIframe(){
-    var iframe = document.createElement('iframe');
-    iframe.src = chrome.extension.getURL('unpaywall.html');
-
-    // styles
-    iframe.style.height = "80px";
-    iframe.style.width = '80px';
-    iframe.style.position = 'fixed';
-    iframe.style.right = '0';
-    iframe.style.top = '33%';
-    iframe.scrolling = 'no';
-    iframe.style.border = '0';
-    iframe.style.zIndex = '9999999999';
-    iframe.style.display = 'none;'
-    iframe.id = "unpaywall";
-
-    //bodyStyle = document.body.style;
-    //cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
-    //position = message.iframe_position;
-
-    return iframe
-}
 
 
 
@@ -82,60 +78,148 @@ function findDoi(){
     // not done yet...improve regex
     var altmetricWidgetDoi = /data-doi\s*=\s*'([^']+)'/;
 
-
     return null
 }
 
-var showTab = function(apiResp) {
-    if (apiResp.oa_color != "green"){
-        devLog("this isn't Green OA. Quitting.")
-        return false
-    }
 
-    var currentUrl = new URL(window.location)
-    var redirectUrl = new URL(apiResp.free_fulltext_url)
-    if (currentUrl.hostname == redirectUrl.hostname){
-        devLog("Looks like we are already on the Green OA source for this article.")
-        return false
-    }
 
-    return true
+function findPdfUrl(){
+    return "http://jcb.rupress.org/content/jcb/191/1/2.2.full.pdf"
 }
 
 
+function insertIframe(name){
+    if (iframeIsInserted){
+        return false // already done, let's quit.
+    }
+    
+    devLog("inserting iframe")
+    iframe.src = chrome.extension.getURL('unpaywall.html');
+    iframe.style.height = "80px";
+    iframe.style.width = '80px';
+    iframe.style.position = 'fixed';
+    iframe.style.right = '0';
+    iframe.style.top = '33%';
+    iframe.scrolling = 'no';
+    iframe.style.border = '0';
+    iframe.style.zIndex = '9999999999';
+    iframe.style.display = 'none;'
+    iframe.id = "unpaywall";
 
-var loadIframe = function(myIframe){
-    var url = "https://api.oadoi.org/" + doi
-    myIframe.onload = function(){
+    // set a custom name
+    iframe.name = name
 
-        $.get(url, function(data){
-            var resp = data.results[0]
-            devLog("got oaDOI info back", resp)
+    document.documentElement.appendChild(iframe);
+    iframeIsInserted = true
+}
 
-            if (showTab(resp)) {
+//function postSuccessMsg(msg){
+//    iframe.contentWindow.postMessage({
+//        unpaywall: "success"
+//    }, "*")
+//}
 
-                // we can't modify the iframe, so send the good news
-                // down as a message
-                myIframe.contentWindow.postMessage({
-                    unpaywall: "success"
-                }, "*")
+function followTabClick(){
+    // we can't tell when someone clicks on the iframe,
+    // so we have to listen to message sent from it.
+    window.addEventListener("message", function(msg){
+        if (msg.data.unpaywall == "go-to-pdf"){
+            devLog("go to pdf", resp.free_fulltext_url)
+            window.location = resp.free_fulltext_url
+        }
+    }, false);
+}
 
-                // we can't tell when someone clicks on the iframe,
-                // so we have to listen to message sent from it.
-                window.addEventListener("message", function(msg){
-                    if (msg.data.unpaywall == "go-to-pdf"){
-                        devLog("go to pdf", resp.free_fulltext_url)
-                        window.location = resp.free_fulltext_url
-                    }
-                }, false);
-            }
-            else {
-                $("#unpaywall").fadeOut()
-            }
-        })
+
+function doPdfScrape(){
+    var pdfUrl = findPdfUrl()
+    if (!pdfUrl){
+        result.pdfScrape.isComplete = true
+        return false
     }
 
+    devLog("doing PDF scrape", pdfUrl)
+
+    // ok, we've got a PDF URL. Let's see if it's open.
+
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", pdfUrl, true)
+    xhr.onprogress = function () {
+        //devLog("HEADERS:", xhr.getAllResponseHeaders())
+        var contentType = xhr.getResponseHeader("Content-Type")
+
+        if (contentType){
+            results.pdfScrape.isComplete = true
+            xhr.abort()
+
+            if (contentType.indexOf("pdf") > -1){
+                devLog("We found a free PDF. This is Gold OA, yay!")
+                results.pdfScrape.url = pdfUrl
+                results.pdfScrape.color = "gold"
+            }
+        }
+    }
+    xhr.send()
 }
+
+function doOadoi(){
+    var doi = findDoi()
+    var url = "https://api.oadoi.org/" + doi + "?email=team@unpaywall.org"
+    devLog("doing oaDOI check", url)
+
+    $.get(url, function(data){
+        results.oadoi.isComplete = true
+        var resp = data.results[0]
+        devLog("got oaDOI info back", resp)
+        if (resp.oa_color){
+            results.oadoi.color = resp.oa_color  // green or gold
+            results.oadoi.url = resp.free_fulltext_url
+        }
+    })
+}
+
+function checkResults(){
+    //devLog("checking results....", results)
+
+    // once all the results are in, we can do stuff
+    if (results.pdfScrape.isComplete && results.oadoi.isComplete){
+
+        // locally scraped pdf gets priority
+        if (results.pdfScrape.color == 'gold'){
+            insertIframe("gold")
+            return true
+        }
+
+        // if no scraped PDF, use the oaDOI response
+        if (results.oadoi.color) {
+            insertIframe(results.oadoi.color)
+            return true
+        }
+    }
+
+    return false
+
+}
+
+function goToFulltext(){
+    var newLoc
+
+    if (results.pdfScrape.url){
+        newLoc = results.pdfScrape.url
+    }
+
+    // if we want to, we can keep the user on the same page here, depending
+    // on the color of oa we are dealing with...no need to reload the page
+    // if the url is just pointing to this same spot, for instance.
+    if (results.oadoi.url){
+        newLoc = results.oadoi.url
+    }
+
+    devLog("sending user to new fulltext URL: ", newLoc)
+    window.location = newLoc
+}
+
+
 
 
 
@@ -143,9 +227,24 @@ var loadIframe = function(myIframe){
 var doi = findDoi()
 if (doi){
     devLog("we have a doi!", doi)
-    var myIframe = createIframe()
-    document.documentElement.appendChild(myIframe);
-    loadIframe(myIframe)
+
+    // these run in parallel:
+    doOadoi()
+    doPdfScrape()
+
+    // todo start polling
+    setInterval(checkResults, 500)
+
+
+    // we can't tell when someone clicks on the iframe,
+    // so we have to listen to message sent from it.
+    window.addEventListener("message", function(msg){
+        if (msg.data.unpaywall == "go-to-pdf"){
+            goToFulltext()
+        }
+    }, false);
+
+
 }
 
 

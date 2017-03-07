0bf00a8bd9bead3e64aea1cfee1a128c46e15cad
Jason Priem <jp@jasonpriem.org>
2017-03-07 14:17:45 -0800

    options get saved on Fx and Chrome.

--- extension/options.js
+++ extension/options.js
@@ -1,36 +1,41 @@
 // Saves options to chrome.storage.sync.
-function save_options() {
 
-    console.log("save options.")
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
-    chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+if (chrome){
+    browser = chrome
+}
+
+
+console.log("loaded options.js")
+function save_options() {
+    console.log("click")
+
+    var showOaColor = document.getElementById('show-oa-color').checked;
+    browser.storage.local.set({
+        showOaColor: showOaColor
+    })
+
+    var status = document.getElementById('status');
+    status.textContent = 'Preference saved.';
+
+    setTimeout(function(){
+        status.textContent = ''
+    }, 1000)
+
+
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
+    browser.storage.local.get({
+        showOaColor: false
     }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        document.getElementById('show-oa-color').checked = items.showOaColor;
     });
 }
 
 
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
+document.getElementById('show-oa-color').addEventListener('click',
     save_options);

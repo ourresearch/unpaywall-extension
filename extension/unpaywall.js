cff282d79f0182ab0220cf4e24e40f479ee57971
Jason Priem <jp@jasonpriem.org>
2018-02-04 11:53:43 -0800

    turn logging back on.

--- extension/unpaywall.js
+++ extension/unpaywall.js
@@ -6,7 +6,7 @@ if (chrome){
 var devLog = function(str, obj){
 
     // turn logging off
-    return false
+    //return false
 
     if (settings && settings.showOaColor){
         console.log("unpaywall: " + str, obj)

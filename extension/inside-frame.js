599bae11e4b6e6293b3b51daba1c50a2478ad027
Richard Orr <richard@impactstory.org>
2021-02-02 13:37:18 -0500

    wrap the icon in an anchor

--- extension/inside-frame.js
+++ extension/inside-frame.js
@@ -6,21 +6,21 @@ var parts = window.name.split("#")
 var color = parts[0]
 var url = decodeURI(parts.slice(1).join('#'))
 
-$(".button")
-    .fadeIn()
-    .click(function(){
+if (color == "black") {
+    $(".button").click(function(){
         // todo: replace this scary alert with a message up to the
         // unpaywall.js content script, which can then pop up
         // a prettier and mre useful dialog box.
-        if (color == "black") {
-            alert("The Unpaywall extension " +
-            "couldn't find any legal open-access version of this article.");
-        }
-        else {
-            // send a message to the extension, so it can open this url
-            // in a new tab
-            browser.runtime.sendMessage({"upUrl": url});
-        }
-    })
+        alert("The Unpaywall extension couldn't find any legal open-access version of this article.");
+    });
+}
+else {
+    $(".button")
+        .wrap($('<a target="_blank" rel="noopener noreferrer"></a>'))
+        .parent()
+        .attr("href", url);
+}
+
+$(".button").fadeIn();
 
 $(".button").addClass(color)
